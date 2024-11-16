6f600a8727bbb017e8f4b2fe78b1cfc192fa238e
Homayoun <homayoun763@gmail.com>
2024-11-16 18:08:40 +0330

    subscribing the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import store from "./store";
 
 const App = () => {
+  const unsubscribe = store.subscribe(() => {
+    console.log('Store Changed', store.getState())
+  })
+
   store.dispatch({
-    type : "bugAdded",
+    type: "bugAdded",
     payload: {
-      description : "Bug1"
+      description: "Bug1"
     }
   })
+
+  unsubscribe()
 
   store.dispatch({
     type: "bugRemoved",
     payload: {
-      id : 1
+      id: 1
     }
   })
   console.log(store.getState())
