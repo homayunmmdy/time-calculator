e2f48f2efd2bff339d1ee2252283526a42b0c018
Homayoun <homayoun763@gmail.com>
2024-11-20 17:23:18 +0330

    custome subscriber

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import store from "./customStore"
 import * as actions from './actions'
 const App = () => {
+  store.subscribe(() => {
+    console.log("Store changed")
+  })
   store.dispatch(actions.bugAdded("Bug 1"))
   console.log(store.getState())
   return (
